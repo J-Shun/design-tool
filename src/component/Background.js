52a206f879d06f7c9b0a90481abc5d8f37ccf383
Shun <ter0123540@gmail.com>
2022-09-06 13:59:21 +0800

    Add functions to make customized color set in Background component

--- src/component/Background.js
+++ src/component/Background.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Tooltip } from "react-tippy";
 import "react-tippy/dist/tippy.css";
 
@@ -16,6 +16,8 @@ const Background = () => {
     backgroundColor: "#FFFFFF",
     textColor: "#000000",
   });
+
+  const [customizedColor, setCustomizedColor] = useState([]);
 
   const copyText = (e) => {
     const copyTarget = e.target.textContent;
@@ -36,6 +38,79 @@ const Background = () => {
     });
   };
 
+  const saveColor = (backgroundColor, textColor) => {
+    const first = localStorage.getItem("first") || null;
+    const second = localStorage.getItem("second") || null;
+    const third = localStorage.getItem("third") || null;
+    if (first === null) {
+      localStorage.setItem("first", `${backgroundColor} ${textColor}`);
+      setCustomizedColor([
+        ...customizedColor,
+        {
+          backgroundColor: backgroundColor,
+          textColor: textColor,
+          name: "first",
+        },
+      ]);
+    } else if (second === null) {
+      localStorage.setItem("second", `${backgroundColor} ${textColor}`);
+      setCustomizedColor([
+        ...customizedColor,
+        {
+          backgroundColor: backgroundColor,
+          textColor: textColor,
+          name: "second",
+        },
+      ]);
+    } else if (third === null) {
+      localStorage.setItem("third", `${backgroundColor} ${textColor}`);
+      setCustomizedColor([
+        ...customizedColor,
+        {
+          backgroundColor: backgroundColor,
+          textColor: textColor,
+          name: "third",
+        },
+      ]);
+    }
+  };
+
+  const deleteColor = () => {
+    localStorage.removeItem("first");
+    localStorage.removeItem("second");
+    localStorage.removeItem("third");
+    setCustomizedColor([]);
+  };
+
+  useEffect(() => {
+    const getCustomizedColor = () => {
+      const first = localStorage.getItem("first") || null;
+      const second = localStorage.getItem("second") || null;
+      const third = localStorage.getItem("third") || null;
+      const colorSet = [];
+      first !== null &&
+        colorSet.push({
+          backgroundColor: first.split(" ")[0],
+          textColor: first.split(" ")[1],
+          name: "first",
+        });
+      second !== null &&
+        colorSet.push({
+          backgroundColor: second.split(" ")[0],
+          textColor: second.split(" ")[1],
+          name: "second",
+        });
+      third !== null &&
+        colorSet.push({
+          backgroundColor: third.split(" ")[0],
+          textColor: third.split(" ")[1],
+          name: "third",
+        });
+      setCustomizedColor(colorSet);
+    };
+    getCustomizedColor();
+  }, []);
+
   return (
     <div
       className="h-screen flex flex-col justify-center items-center ease-linear duration-300"
@@ -47,7 +122,7 @@ const Background = () => {
       >
         Color Matching
       </h2>
-      <div className="flex justify-between w-52 mb-10">
+      <div className="flex justify-between w-52 mb-5">
         <div className="flex flex-col items-center w-20">
           <label
             className="mb-2 w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
@@ -119,7 +194,36 @@ const Background = () => {
         </div>
       </div>
 
-      <ol className="flex gap-4">
+      <div className="flex gap-3">
+        <button
+          className="mb-7 py-2 px-4 rounded-lg"
+          style={{
+            color: color.textColor,
+            backgroundColor: color.backgroundColor,
+            border: `1px solid ${color.textColor}`,
+          }}
+          onClick={() => {
+            saveColor(color.backgroundColor, color.textColor);
+          }}
+        >
+          Save
+        </button>
+        <button
+          className="mb-7 py-2 px-4 rounded-lg"
+          style={{
+            color: color.textColor,
+            backgroundColor: color.backgroundColor,
+            border: `1px solid ${color.textColor}`,
+          }}
+          onClick={() => {
+            deleteColor();
+          }}
+        >
+          Delete
+        </button>
+      </div>
+
+      <ol className="flex gap-4 mb-5">
         {colorSet.map((set) => {
           return (
             <li
@@ -148,6 +252,33 @@ const Background = () => {
           );
         })}
       </ol>
+
+      {customizedColor.length > 0 && (
+        <ol className="flex gap-4">
+          {customizedColor.map((set) => {
+            return (
+              <li
+                className="flex flex-col justify-center items-center cursor-pointer"
+                data-bg={set.backgroundColor}
+                data-text={set.textColor}
+                onClick={(e) => renderColor(e)}
+                key={set.name}
+              >
+                <div
+                  className="flex justify-center items-center w-8 h-8 bg-white rounded-full mb-2 pointer-events-none"
+                  style={{ backgroundColor: set.backgroundColor }}
+                >
+                  <div
+                    className="w-5 h-5 bg-black rounded-full pointer-events-none"
+                    style={{ backgroundColor: set.textColor }}
+                  ></div>
+                </div>
+                <p>{set.name}</p>
+              </li>
+            );
+          })}
+        </ol>
+      )}
     </div>
   );
 };
