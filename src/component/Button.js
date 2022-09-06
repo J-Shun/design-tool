2749e7d9ccd2f8f99b03b4ec1a9fb13e8c0908cb
Shun <ter0123540@gmail.com>
2022-09-06 11:28:14 +0800

    Add functions to control button in Button component

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { TbBorderRadius, TbBorderOuter } from "react-icons/tb";
 
 const Button = () => {
   const [word, setWord] = useState("Button");
 
   const [color, setColor] = useState({
+    textColor: "#FFFFFF",
+    backgroundColor: "#000000",
+  });
+
+  const [showHover, setShowHover] = useState(false);
+  const [hoverColor, setHoverColor] = useState({
     textColor: "#FFFFFF",
     backgroundColor: "#000000",
   });
@@ -50,13 +57,25 @@ const Button = () => {
     }
   };
 
+  const handleHoverColor = (e) => {
+    if (showHover === false) return;
+    const moveType = e.type;
+    if (moveType === "mouseenter") {
+      e.target.style.backgroundColor = hoverColor.backgroundColor;
+      e.target.style.color = hoverColor.textColor;
+    } else {
+      e.target.style.backgroundColor = color.backgroundColor;
+      e.target.style.color = color.textColor;
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h2 className="text-5xl mb-10">Button</h2>
       <div className="flex justify-between container mx-auto">
         <div className="flex-1 flex justify-center items-center">
           <button
-            className="cursor-pointer"
+            className="ease-linear duration-200 cursor-pointer"
             style={{
               backgroundColor: color.backgroundColor,
               color: color.textColor,
@@ -64,16 +83,24 @@ const Button = () => {
               paddingInline: `${padding.x}px`,
               borderRadius: `${allBorderRadius.topLeft}% ${allBorderRadius.topRight}% ${allBorderRadius.bottomRight}% ${allBorderRadius.bottomLeft}%`,
             }}
+            onMouseEnter={(e) => {
+              handleHoverColor(e);
+            }}
+            onMouseLeave={(e) => {
+              handleHoverColor(e);
+            }}
           >
             {word}
           </button>
         </div>
-        <div className="flex-1 flex justify-center items-center">
+        <div className="flex-1 flex justify-center items-center text-lg">
           <div className="flex flex-col mx-auto text-center">
-            <label htmlFor="text">Text</label>
+            <label htmlFor="text" className="mb-2">
+              Text
+            </label>
             <input
               type="text"
-              className="border p-2"
+              className="border p-2 mb-4"
               id="text"
               value={word}
               placeholder="Text Area"
@@ -81,31 +108,41 @@ const Button = () => {
                 write(e);
               }}
             />
-            <label htmlFor="padding-x">Padding X: {padding.x}</label>
-            <input
-              type="range"
-              id="padding-x"
-              name="x"
-              min="0"
-              max="50"
-              value={padding.x}
-              onChange={(e) => {
-                changePadding(e);
-              }}
-            />
-            <label htmlFor="padding-y">Padding Y: {padding.y}</label>
-            <input
-              type="range"
-              id="padding-y"
-              name="y"
-              min="0"
-              max="50"
-              value={padding.y}
-              onChange={(e) => {
-                changePadding(e);
-              }}
-            />
-            <label>
+            <div className="flex justify-between mb-4">
+              <div className="flex flex-col">
+                <label htmlFor="padding-x" className="mb-2">
+                  Padding X: {padding.x}
+                </label>
+                <input
+                  type="range"
+                  id="padding-x"
+                  name="x"
+                  min="0"
+                  max="50"
+                  value={padding.x}
+                  onChange={(e) => {
+                    changePadding(e);
+                  }}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="padding-y" className="mb-2">
+                  Padding Y: {padding.y}
+                </label>
+                <input
+                  type="range"
+                  id="padding-y"
+                  name="y"
+                  min="0"
+                  max="50"
+                  value={padding.y}
+                  onChange={(e) => {
+                    changePadding(e);
+                  }}
+                />
+              </div>
+            </div>
+            <label className="mb-2">
               Border-All:
               <input
                 type="checkbox"
@@ -117,9 +154,12 @@ const Button = () => {
             </label>
             {showAll ? (
               <>
-                <label>Border-radius: {allBorderRadius.all}%</label>
+                <label className="flex justify-center items-center mb-2">
+                  <TbBorderOuter className="w-6 h-6" />: {allBorderRadius.all}%
+                </label>
                 <input
                   type="range"
+                  className="mb-4"
                   name="all"
                   min="0"
                   max="50"
@@ -131,9 +171,12 @@ const Button = () => {
               </>
             ) : (
               <>
-                <div className="flex">
+                <div className="flex  justify-between mb-4">
                   <div className="flex flex-col">
-                    <label>Top-Left: {allBorderRadius.topLeft} %</label>
+                    <label className="flex justify-center items-center mb-2">
+                      <TbBorderRadius className="w-6 h-6" />:{" "}
+                      {allBorderRadius.topLeft} %
+                    </label>
                     <input
                       type="range"
                       name="topLeft"
@@ -146,7 +189,10 @@ const Button = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label>Top-Right: {allBorderRadius.topRight} %</label>
+                    <label className="flex justify-center items-center mb-2">
+                      <TbBorderRadius className="w-6 h-6 rotate-90" />:{" "}
+                      {allBorderRadius.topRight} %
+                    </label>
                     <input
                       type="range"
                       name="topRight"
@@ -159,9 +205,12 @@ const Button = () => {
                     />
                   </div>
                 </div>
-                <div className="flex">
+                <div className="flex justify-between mb-4">
                   <div className="flex flex-col">
-                    <label>Bottom-Left: {allBorderRadius.bottomLeft} %</label>
+                    <label className="flex justify-center items-center mb-2">
+                      <TbBorderRadius className="w-6 h-6 -rotate-90" />:{" "}
+                      {allBorderRadius.bottomLeft} %
+                    </label>
                     <input
                       type="range"
                       name="bottomLeft"
@@ -174,7 +223,10 @@ const Button = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label>Bottom-Right: {allBorderRadius.bottomRight} %</label>
+                    <label className="flex justify-center items-center mb-2">
+                      <TbBorderRadius className="w-6 h-6 rotate-180" />:{" "}
+                      {allBorderRadius.bottomRight} %
+                    </label>
                     <input
                       type="range"
                       name="bottomRight"
@@ -189,39 +241,85 @@ const Button = () => {
                 </div>
               </>
             )}
-            <div>Background Color</div>
-            <label
-              className="mb-2 mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
-              style={{ backgroundColor: color.backgroundColor }}
-            >
+            <label htmlFor="hover" className="mb-2">
+              Set Hover:
               <input
-                className="invisible"
-                type="color"
-                onInput={(e) => {
-                  setColor({
-                    ...color,
-                    backgroundColor: e.target.value,
-                  });
-                }}
-                value={color.backgroundColor}
-              />
-            </label>
-            <div>Text Color</div>
-            <label
-              className="mb-2 mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
-              style={{ backgroundColor: color.textColor }}
-            >
-              <input
-                className="invisible"
-                type="color"
-                onInput={(e) => {
-                  setColor({
-                    ...color,
-                    textColor: e.target.value,
-                  });
+                type="checkbox"
+                className="ml-2"
+                id="hover"
+                onClick={() => {
+                  setShowHover(!showHover);
                 }}
               />
             </label>
+            <div className="flex mb-4">
+              <label
+                className="mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
+                style={{ backgroundColor: color.backgroundColor }}
+              >
+                <input
+                  className="invisible"
+                  type="color"
+                  onInput={(e) => {
+                    setColor({
+                      ...color,
+                      backgroundColor: e.target.value,
+                    });
+                  }}
+                  value={color.backgroundColor}
+                />
+              </label>
+              <label
+                className="mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
+                style={{ backgroundColor: color.textColor }}
+              >
+                <input
+                  className="invisible"
+                  type="color"
+                  onInput={(e) => {
+                    setColor({
+                      ...color,
+                      textColor: e.target.value,
+                    });
+                  }}
+                />
+              </label>
+            </div>
+            {showHover && (
+              <div className="flex ">
+                <label
+                  className="mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
+                  style={{ backgroundColor: hoverColor.backgroundColor }}
+                >
+                  <input
+                    className="invisible"
+                    type="color"
+                    onInput={(e) => {
+                      setHoverColor({
+                        ...hoverColor,
+                        backgroundColor: e.target.value,
+                      });
+                    }}
+                    value={hoverColor.backgroundColor}
+                  />
+                </label>
+                <label
+                  className="mx-auto w-10 h-10 overflow-hidden rounded-full border-2 border-gray-400 cursor-pointer"
+                  style={{ backgroundColor: hoverColor.textColor }}
+                >
+                  <input
+                    className="invisible"
+                    type="color"
+                    onInput={(e) => {
+                      setHoverColor({
+                        ...hoverColor,
+                        textColor: e.target.value,
+                      });
+                    }}
+                  />
+                </label>
+              </div>
+            )}
           </div>
         </div>
       </div>
